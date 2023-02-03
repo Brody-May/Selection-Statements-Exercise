1ac2980ae252bf17c4a5ca0a0368358f8aea94ba
Brody May <brodymay2019@outlook.com>
2023-02-03 12:43:46 -0500

    exercise changes

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favoriteNumber = 9;
+            Console.WriteLine("Try to guess my favorite number from 1-10");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favoriteNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favoriteNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it");
+            }
+        
+            //Switch Case
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+            switch (subject.ToLower()) 
+            {
+                case "math":
+                    Console.WriteLine("My favorite too!");
+                    break;
+                case "science":
+                    Console.WriteLine("Eh mediocre at best");
+                    break;
+                case "english":
+                    Console.WriteLine("I hate english! >:(");
+                    break;
+                case "pe":
+                    Console.WriteLine("Pretty good choice");
+                    break;
+                case "history":
+                    Console.WriteLine("My second least favorite");
+                    break;
+                default:
+                    Console.WriteLine("I don't like that answer");
+                    break;
+
+
+
+            }
+        
         }
+
     }
 }
